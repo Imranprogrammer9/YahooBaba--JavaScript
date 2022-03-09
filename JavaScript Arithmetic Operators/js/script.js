60bf80b37605b188e87b13d02335f27625a50532
Imran.Programmer <90908160+Imranprogrammer9@users.noreply.github.com>
2022-03-09 17:00:07 +0500

    Added JavaScript Arithmetic Operators

---
+++ JavaScript Arithmetic Operators/js/script.js
@@ -0,0 +1,58 @@
+// Arithmetic Operators in JavaScript
+
+// 1. Addition
+var a = 10;
+var b = 20;
+document.write(a + b);
+document.write("<br><br>");
+
+//2. Subtraction
+var c = 30;
+var d = 40;
+document.write(c - d);
+document.write("<br><br>");
+
+//3. Multiplication
+var e = 50;
+var f = 60;
+document.write(e * f);
+document.write("<br><br>");
+
+//4. Exponentiation
+var g = 10;
+var h = 5;
+document.write(g ** h);
+document.write("<br><br>");
+
+//5. Increment
+var i = 10;
+var j = 11;
+// Before Increment
+document.write(i + j);
+// After Increment
+i++;
+document.write("<br><br>");
+document.write(i + j);
+
+//6. Decrement
+var k = 12;
+var l = 13;
+//Before Decrement
+document.write("<br><br>");
+document.write(k + i);
+// After Decrement
+l--;
+document.write("<br><br>");
+document.write(k + l);
+
+//7. Division
+var m = 14;
+var n = 2;
+document.write("<br><br>");
+document.write(m / 2);
+
+//8. Modulus
+var o = 4;
+var p = 2;
+document.write("<br><br>");
+document.write(o % p);
