0a0293c6e12c0500d9367defcc522cc0c36c9945
Imran.Programmer <90908160+Imranprogrammer9@users.noreply.github.com>
2022-03-09 12:53:42 +0500

    Update Script.js in Comments in JavaScript

--- Comments in JavaScript/js/script.js
+++ Comments in JavaScript/js/script.js
@@ -1,4 +1,5 @@
-// Commands Used For Printing Anything
+// Commands Used For Printing Anything -- Single line Comment
+// Multiline Comment
 /* This tutorial is explaining the use of HTML tags in JavaScript. Following are the examples for understanding of using Tags in JavaScript */
 document.write("<b>Imran Haider</b><br><br>");
 document.write("<i>Assitant Operation Manager / Developer</i><br><br>");
