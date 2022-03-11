c1ab5a516af44501b139dc03bc896b306b96e812
Imran.Programmer <90908160+Imranprogrammer9@users.noreply.github.com>
2022-03-11 12:50:39 +0500

    Assignment Operators

---
+++ Assignment Operators in JavaScript/js/script.js
@@ -0,0 +1,33 @@
+//With Arithmetic Operators
+var a = 10;
+var b = 20;
+var c = a + b;
+document.write(c);
+document.write("<br>");
+document.write("<br>");
+
+//With Assignment Operators
+
+// Addition
+a += b;
+document.write(a);
+document.write("<br>");
+document.write("<br>");
+
+// Subtraction
+a -= b;
+document.write(a);
+document.write("<br>");
+document.write("<br>");
+
+// Multiply
+a *= b;
+document.write(a);
+document.write("<br>");
+document.write("<br>");
+
+// Modulus
+a %= b;
+document.write(a);
+document.write("<br>");
+document.write("<br>");
